4a3ef23e4bfdfad4e75515bd9bf88deb4150592d
William Augusto Palacios Hernández <williampalacios@localhost.localdomain>
2021-04-27 11:53:50 -0500

    QUER-1 renombrar app mockups a queridocuerpo_app

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mockups.urls')),
+    path('', include('queridocuerpo_app.urls')),
 ]
