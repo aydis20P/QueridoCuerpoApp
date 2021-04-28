eaf78ca01af3aa52d516b83a2f43e869cfbb29b3
William Augusto Palacios Hernández <williampalacios@localhost.localdomain>
2021-04-28 15:35:02 -0500

    QUER-16 plantilla principal profesional de la salud terminada

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('queridocuerpo_app.urls')),
+    path('pro/', include('queridocuerpo_pro.urls')),
 ]
