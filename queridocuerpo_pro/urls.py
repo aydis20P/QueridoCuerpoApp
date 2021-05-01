5a1cef8b8e3521d376e80d91903fcce6f988ea92
William Augusto Palacios Hernández <williampalacios@localhost.localdomain>
2021-05-01 09:54:22 -0500

    primer acercamineto platilla de resumen-usurio pro (index.html)

--- queridocuerpo_pro/urls.py
+++ queridocuerpo_pro/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.principal, name="principal")
+    path('', views.principal, name="principal"),
+    path('resumen-usuario', views.resumen_usuario, name="resumen-usuario")
 ]
--- queridocuerpo_pro/views.py
+++ queridocuerpo_pro/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def principal(request):
     return render(request, 'queridocuerpo_pro/principal.html')
+
+def resumen_usuario(request):
+    return render (request, 'queridocuerpo_pro/index.html')
