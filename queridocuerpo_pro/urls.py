68ca435d4fb958183c752cc66783156b7af029b2
William Augusto Palacios Hernández <williampalacios@localhost.localdomain>
2021-05-07 19:40:17 -0500

    planes actualizados acorde a los servicios de QC

--- queridocuerpo_pro/urls.py
+++ queridocuerpo_pro/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.principal, name="principal"),
-    path('resumen-usuario', views.resumen_usuario, name="resumen-usuario")
+    path('resumen-usuario', views.resumen_usuario, name="resumen-usuario"),
+    path('perfil-profesional', views.perfil_profesional, name="perfil-profesional"),
+    path('perfil-miembro', views.perfil_miembro, name="perfil-miembro"),
+    path('plan-alimenticio-miembro', views.plan_alimenticio_miembro, name="plan-alimenticio-miembro"),
+    path('resumen-usuario-administrativo', views.resumen_usuario_administrativo, name="resumen-usuario-administrativo"),
 ]
